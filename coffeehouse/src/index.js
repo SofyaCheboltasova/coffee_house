6e06fab9e485939effb80ebf86bb1548c60c05ee
Sofya Cheboltasova <s.cheboltasova@g.nsu.ru>
2024-02-08 16:58:54 +0700

    feat: create 'about' block

--- coffeehouse/src/index.js
+++ coffeehouse/src/index.js
@@ -3,12 +3,14 @@ import "./sass/main.scss";
 import createHeader from "./scripts/view/main/header";
 import createMain from "./scripts/view/main/main";
 import createFavourite from "./scripts/view/main/favorite";
+import createAbout from "./scripts/view/main/about";
 
 createHeader();
 const main = createMain();
 const favorite = createFavourite();
+const about = createAbout();
 
 const mainBlock = document.createElement("main");
 mainBlock.classList.add("main-block");
-mainBlock.append(main, favorite);
+mainBlock.append(main, favorite, about);
 document.body.appendChild(mainBlock);
---
+++ coffeehouse/src/scripts/view/main/about.js
@@ -0,0 +1,58 @@
+function createGallery(aboutWrapper) {
+  const imgWrapper1 = document.createElement("div");
+  imgWrapper1.classList.add("about__img-wrapper");
+
+  for (let i = 0; i < 2; i += 1) {
+    const img = document.createElement("img");
+    img.src = `./assets/about/about-${i + 1}.png`;
+    img.classList.add("about__img");
+    if (i === 1) {
+      img.classList.add("about__img_short");
+    }
+    imgWrapper1.appendChild(img);
+  }
+
+  const imgWrapper2 = document.createElement("div");
+  imgWrapper2.classList.add("about__img-wrapper");
+
+  for (let i = 2; i < 4; i += 1) {
+    const img = document.createElement("img");
+    img.src = `./assets/about/about-${i + 1}.png`;
+    img.classList.add("about__img");
+    if (i === 2) {
+      img.classList.add("about__img_short");
+    }
+    imgWrapper2.appendChild(img);
+  }
+
+  aboutWrapper.append(imgWrapper1, imgWrapper2);
+  return aboutWrapper;
+}
+
+function createAbout() {
+  const about = document.createElement("section");
+  const aboutWrapper = document.createElement("div");
+  about.classList.add("about");
+  aboutWrapper.classList.add("about__wrapper");
+
+  const h1 = document.createElement("h1");
+  const accent = document.createElement("span");
+  const textStart = document.createElement("span");
+  const textEnd = document.createElement("span");
+
+  accent.classList.add("text__accent");
+  accent.textContent = " the perfect and cozy place ";
+  textStart.textContent = "Resource is";
+  textEnd.textContent =
+    "where you can enjoy a variety of hot beverages, relax, catch up with friends, or get some work done.";
+
+  h1.classList.add("text__dark");
+  h1.append(textStart, accent, textEnd);
+
+  const gallery = createGallery(aboutWrapper);
+  about.append(h1, gallery);
+  return about;
+}
+
+export default createAbout;
+
