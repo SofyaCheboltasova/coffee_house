4c99387f9fcb982b36d9517a992bb12573d01db0
Sofya Cheboltasova <s.cheboltasova@g.nsu.ru>
2024-02-19 19:05:10 +0700

    feat: add media queries for modal

--- coffeehouse/src/scripts/menu/pageTemplates/modal.js
+++ coffeehouse/src/scripts/menu/pageTemplates/modal.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-console */
-/* eslint-disable prefer-destructuring */
 /* eslint-disable no-restricted-syntax */
 async function getProductInfo(productId) {
   const response = await fetch("./assets/dataJson/products.json");
